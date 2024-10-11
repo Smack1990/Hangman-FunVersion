5de73e62de603a45716b67dc30ada74b7eb89249
Smack1990 <johansson.t.markus@gmail.com>
2024-10-11 08:54:17 +0200

    Add project files.

---
+++ Hangman basic/Player.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hangman_basic;
+internal class Player
+{
+    public string PlayerName { get; private set; }
+    public int Score { get; set; }
+    public List<char> guessedLetters = new List<char>();
+    public Player(string name)
+    {
+        PlayerName = name;
+        guessedLetters = new List<char>();  
+        Score = 0;
+
+    }
+
+
+}
---
+++ Hangman basic/Words.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Hangman_basic;
+internal class Words
+{
+    const string JsonWords = @"MyWords.json"; 
+    public List<string> WordList { get; set; }
+
+    public Words()
+    {
+        WordList = new List<string>();
+        ReadJson(); 
+    }
+
+    public string GetFilePath(string filename)
+    {
+        string filePath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+        return Path.Combine(filePath, filename);
+    }
+
+    public void WriteJson(string input)
+    {
+        if (!string.IsNullOrEmpty(input))
+        {
+            WordList.Add(input);
+        }
+        string json = JsonSerializer.Serialize(WordList);
+        var filePath = GetFilePath(JsonWords);
+        File.WriteAllText(filePath, json);
+        
+    }
+
+    public void ReadJson()
+    {
+        string filePath = GetFilePath(JsonWords);
+        if (File.Exists(filePath)) 
+        {
+            string json = File.ReadAllText(filePath);
+            WordList = JsonSerializer.Deserialize<List<string>>(json)!;
+            //foreach (var word in WordList) //Kontrollera om listan finns eller ej. 
+            //{
+            //    Console.WriteLine(word);
+            //}
+        }
+        else
+        {
+            WordList = new List<string> { "apple", "banana", "cherry", "date", "elderberry", "fig", "grape", "honeydew", "kiwi", "lemon", "mango", "nectarine", "orange", "pear", "quince", "raspberry", "strawberry", "tangerine", "watermelon" };
+            
+        }
+    }
+}
